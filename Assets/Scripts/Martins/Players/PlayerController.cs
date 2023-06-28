a5e39de7c6451f4365fd45f7ae9570dec5c66a05
abegao <lucasbaptista567@gmail.com>
2023-06-27 21:35:21 -0300

    att do cenario

--- Assets/Scripts/Martins/Players/PlayerController.cs
+++ Assets/Scripts/Martins/Players/PlayerController.cs
@@ -15,17 +15,53 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;
 
     public SpriteRenderer fliparPerson;
-
+    public GameObject painelJogo;
+    public GameObject painelJogo2;
+    public GameObject painelJogo3;
+    public GameObject painelJogo4;
+    public GameObject painelJogo5;
     public GameObject menuPanel;
     private bool isMenuOpen = false;
+
+   
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("aa"))
         {
-            SceneManager.LoadScene("Gameplay");
+            AbrirPainel();
+            PausarJogo();
+
         }
+        if (other.CompareTag("tartaruga"))
+        {
+            AbrirPainel2();
+            PausarJogo();
+
+        }
+
+        if (other.CompareTag("macaco"))
+        {
+            AbrirPainel3();
+            PausarJogo();
+
+        }
+
+         if (other.CompareTag("sagui"))
+        {
+            AbrirPainel4();
+            PausarJogo();
+
+        }
+
+        if (other.CompareTag("papagaio"))
+        {
+            AbrirPainel5();
+            PausarJogo();
+
+        }
+
     }
     // private void OnTriggerEnter2D(Collider2D other)
     // {
@@ -94,8 +130,70 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+       
+
 
     }
+
+        public void AbrirPainel()
+        {
+            painelJogo.SetActive(true);
+        }
+
+        public void FecharPainel()
+        {
+            painelJogo.SetActive(false);
+        }
+
+        private void PausarJogo()
+       {
+            Time.timeScale = 0f;
+       }
+
+        public void RetomarJogo()
+       {
+        Time.timeScale = 1f;
+       }
+
+       public void AbrirPainel2()
+       {
+            painelJogo2.SetActive(true);
+       }
+
+        public void FecharPainel2()
+        {
+        painelJogo2.SetActive(false);
+        }
+
+         public void AbrirPainel3()
+        {
+            painelJogo3.SetActive(true);
+        }
+
+        public void FecharPainel3()
+        {
+            painelJogo3.SetActive(false);
+        }
+
+        public void AbrirPainel4()
+        {
+            painelJogo4.SetActive(true);
+        }
+
+        public void FecharPainel4()
+        {
+            painelJogo4.SetActive(false);
+        }
+
+        public void AbrirPainel5()
+        {
+            painelJogo5.SetActive(true);
+        }
+
+        public void FecharPainel5()
+        {
+            painelJogo5.SetActive(false);
+        }
 }
 
 
